0fdfce39da1efaff94999a8d72c6899996863728
DESKTOP-M6JVSEJ\Salih Demir <salihdemir.58@gmail.com>
2018-04-28 21:01:38 +0300

    Node graph added to the homepage.

--- src/main/java/com/linkyway/model/entity/Node.java
+++ src/main/java/com/linkyway/model/entity/Node.java
@@ -1,6 +1,7 @@
 package com.linkyway.model.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.neo4j.ogm.annotation.GraphId;
 import org.neo4j.ogm.annotation.NodeEntity;
 
@@ -9,15 +10,14 @@ import javax.persistence.GeneratedValue;
 /**
  * @author huseyin.kilic
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NodeEntity
 public class Node extends AbstractEntity {
-
     @GraphId
     @GeneratedValue
     private Long id;
     private String name;
     private String type;
     private String description;
-
 }
